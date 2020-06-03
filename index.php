42615722e0f271bf665ec8b568476398ea79ea4c
КуДАХТЕР НАМБЕР 1 <naixwexexort@gmail.com>
2020-06-03 15:06:29 +0600

    123

---
+++ index.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+$rows = 10;
+$cols = 10; 
+
+$table = '<table border="1">';
+
+for ($tr=1; $tr<=$rows; $tr++){
+    $table .= '<tr>';
+    for ($td=1; $td<=$cols; $td++){
+        if ($tr===1 or $td===1){
+            $table .= '<th style="color:white;background-color:LightGreen;">'. $tr*$td .'</th>'; 
+        }else{
+            $table .= '<td>'. $tr*$td .'</td>';
+        }
+    }
+    $table .= '</tr>';
+}
+
+$table .= '</table>';
+echo $table;
+
+?>
---
+++ table.php
@@ -0,0 +1,17 @@
+﻿<?php
+echo("<table>\n");
+for($i=0;$i<=10;$i++)
+{
+   echo("<tr>\n");
+   for($j=0;$j<=10;$j++)
+   {
+      echo("<td>\n");
+      echo(($i+$j));
+      echo("</td>\n");
+   }
+   echo("</tr>\n");
+}
+echo("</table>\n");
+
+
+?>
